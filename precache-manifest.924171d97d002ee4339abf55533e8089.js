cb781687e6ab11307ed51844a23acbc0ad4a8dd4
mmqn <michaelng728@gmail.com>
2019-11-06 17:37:44 -0800

    Updates

---
+++ precache-manifest.924171d97d002ee4339abf55533e8089.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f0d92957a2728401049e6bab9af2982",
+    "url": "/wanderer/index.html"
+  },
+  {
+    "revision": "872f5b4276f8b9176d13",
+    "url": "/wanderer/static/css/main.c8ed31f6.chunk.css"
+  },
+  {
+    "revision": "5c9ff16ec5bee154aee2",
+    "url": "/wanderer/static/js/2.5286435d.chunk.js"
+  },
+  {
+    "revision": "872f5b4276f8b9176d13",
+    "url": "/wanderer/static/js/main.7f4f3fd6.chunk.js"
+  },
+  {
+    "revision": "2d9e58f4dcfe1ec4a0ba",
+    "url": "/wanderer/static/js/runtime~main.fbb736d4.js"
+  }
+]);
